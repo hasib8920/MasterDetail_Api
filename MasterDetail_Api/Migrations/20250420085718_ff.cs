7c411f4ed9538befb5f16d0436a6d88da73e0d25
kamrul1999azad <kamrulazad2923@gmail.com>
2025-04-23 11:45:01 +0600

    Add project files.

---
+++ MasterDetail_Api/Migrations/20250420085718_ff.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MasterDetail_Api.Migrations
+{
+    public partial class ff : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Clients",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
---
+++ MasterDetail_Api/Migrations/20250421063306_oo.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MasterDetail_Api.Migrations
+{
+    public partial class oo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
